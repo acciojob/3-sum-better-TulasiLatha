467b3353b2b57d6e5a0c057f07c4063f849d9583
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-03 11:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	var sum=0;
+  for(var i=0;i<arr.length;i++){
+	  sum=sum+arr[i];
+	  if(sum>=target){
+		  console.log("The sum is closest to the target is"+sum);
+	  }
+  }
 }
 
 module.exports = threeSum;
